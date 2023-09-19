783ef991b4a802f33eef00afdfe9534320e1260e
badvibes4everr <141183104+badvibes4everr@users.noreply.github.com>
2023-09-18 23:58:52 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,14 +35,60 @@ if(navMenu && navCloseBtn){
 }
 
 // Change header bg color
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
 
+  if(scrollY > 5){
+    document.querySelector("header").classList.add("header-active");
+  }else{
+    document.querySelector("header").classList.remove("header-active");
+  }
 
 // Scroll up button
-  
+  const scrollUpBtn = document.querySelector('.scrollUp-btn');
+  if(scrollY > 250){
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  }else{
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
   
   
 // Nav link indicator
- 
-  
+
+const sections = document.querySelectorAll('section[id]');
+console.log(sections)
+sections.forEach(section =>{
+  const sectionHeight = section.offsetHeight,
+        sectionTop = section.offsetTop - 60;
+
+
+        let navId = document.querySelector(`.menu-content a[href= '#${section.id}']`);
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          navId.classList.add("active-navlink");
+        }else{
+          navId.classList.remove("active-navlink");
+        }
+
+        navId.addEventListener("click" , () => {
+          navMenu.classList.remove("open");
+          body.style.overflowY = "scroll";
+        })
+
+ })
+})
+
+
 // Scroll Reveal Animation
   
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.section-subtitle, .section-title, .section-description, .brand-image, .testimonial, .newsletter, .logo-content, .newsletter-inputbox, .newsletter-mediaIcon, .footer-content, .footer-links`, {interval:100,})
+
+sr.reveal(` .about-imageContent, .menu-items`, {origin: 'left'})
+
+sr.reveal(` .about-details, .time-table`, {origin: 'right'})
